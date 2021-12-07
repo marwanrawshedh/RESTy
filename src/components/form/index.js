9ad4c2d6859957734b920172db6cdfcfcdb2dd95
marwanrawshedh <marwansalem55@yahoo.com>
2021-12-07 14:50:23 +0200

    fixing lab 27

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -13,50 +13,58 @@ function Form(props) {
     props.handleApiCall(formData);
   };
   return (
-    <>
+    // <>
+    <div >
       <form onSubmit={handleSubmit}>
         <label>
           <span>URL: </span>
           <input name="url" type="text" defaultValue="https://jsonplaceholder.typicode.com/todos" />
-          <button data-testid="click-form" type="submit">GO!</button>
+          <button data-testid="click-form" className="button-64" type="submit">GO!</button>
         </label>
       </form>
+      <textarea className="textarea" name="textarea" rows="4" cols="50">
+
+  </textarea>
       <section className="methods">
         <button
-          className="b"
+          className="button-64"
           onClick={() => {
             setMethods((methods) => "GET");
           }}
         >
-          GET
+          <span class="text">GET</span>
+          
         </button>
         <button
-          className="b"
+          className="button-64"
           onClick={() => {
             setMethods((methods) => "POST");
           }}
         >
-          POST
+          <span class="text">POST</span>
+          
         </button>
         <button
-          className="b"
+          className="button-64"
           onClick={() => {
             setMethods((methods) => "PUT");
           }}
           id="PUT"
         >
-          PUT
+          <span class="text"> PUT</span>
+         
         </button>
         <button
-          className="b"
+          className="button-64"
           onClick={() => {
             setMethods((methods) => "DELETE");
           }}
         >
-          DELETE
+          <span class="text">DELETE</span>
+          
         </button>
       </section>
-    </>
+    </div>
   );
 }
 
